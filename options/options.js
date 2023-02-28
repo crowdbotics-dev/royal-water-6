42fc66310f3f0c647f6a961a1c738d93d42ca1d7
Crowdbotics <engineering@crowdbotics.com>
2023-02-28 00:24:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-water-6",
+  url: "https://royal-water-6.botics.co",
+  api: "https://royal-water-6.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
